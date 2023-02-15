face07e1be9fb00335b87caadbe6af9a695f0e13
mouadcherkaoui <cherkaoui.mouad@live.fr>
2023-02-15 10:18:08 +0100

    UP: adding expense item/list component logic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,41 @@
 
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { RiAddCircleFill } from 'react-icons/ri';
+import { AppContext } from '../context/AppContext';
+
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = (ev) => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+        return (
+                <tr>
+                <td>{props.name}</td>
+                <td>£{props.cost}</td>
+                <td><RiAddCircleFill onClick={event=> increaseAllocation(props.name)}/></td>
+                <td><TiDelete size='1.5em' onClick={event => handleDeleteExpense(event)}/></td>
+                </tr>
+        );
+};
+
+export default ExpenseItem;
+
