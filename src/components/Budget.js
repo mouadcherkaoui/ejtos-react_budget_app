141dabf760df8506ce6c72359fd435630041941b
mouadcherkaoui <cherkaoui.mouad@live.fr>
2023-02-15 10:17:13 +0100

    UP: adding budget component logic

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,24 @@
 
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { currentBudget } = useContext(AppContext);
+    const [ budget, setBudget] = useState(currentBudget);
+    //let setCost = cost => budget = cost;
+    return (
+        <div className='alert alert-secondary'>
+                <input
+                        required='required'
+                        type='number'
+                        id='cost'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
